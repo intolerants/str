bafa9c309894c2a1e8b94fe1d748dc3017e95dfc
Hanoch Griner <eugriner@gmail.com>
2016-05-01 20:57:17 -0300

    ADD - update datetime linux

---
+++ beagleSocket/systemTimeUpdate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# add in /etc/rc.local
+
+( /etc/init.d/ntp stop
+until ping -nq -c3 8.8.8.8; do
+   echo "Waiting for network..."
+done
+ntpdate -s time.nist.gov
+/etc/init.d/ntp start )&
